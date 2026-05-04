a504e5208a0d0077b21343010fda17af7ccd6f67
Johan Cwiklinski <johan@x-tnd.be>
2026-05-04 19:17:15 +0200

    No longer rely on global

--- _routes.php
+++ _routes.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 
 use GaletteActivities\Controllers\Crud\ActivitiesController;
 use GaletteActivities\Controllers\Crud\SubscriptionsController;
+use Galette\Middleware\Authenticate;
 
 //Constants and classes from plugin
 require_once $module['root'] . '/_config.inc.php';
@@ -30,66 +31,66 @@ require_once $module['root'] . '/_config.inc.php';
 $app->get(
     '/activities[/{option:page|order}/{value:\d+}]',
     [ActivitiesController::class, 'list']
-)->setName('activities_activities')->add($authenticate);
+)->setName('activities_activities')->add(Authenticate::class);
 
 $app->get(
     '/activity/add',
     [ActivitiesController::class, 'add']
 )->setName(
     'activities_activity_add'
-)->add($authenticate);
+)->add(Authenticate::class);
 
 $app->get(
     '/activity/edit/{id:\d+}',
     [ActivitiesController::class, 'edit']
 )->setName(
     'activities_activity_edit'
-)->add($authenticate);
+)->add(Authenticate::class);
 
 $app->post(
     '/activity/add',
     [ActivitiesController::class, 'doAdd']
-)->setName('activities_storeactivity_add')->add($authenticate);
+)->setName('activities_storeactivity_add')->add(Authenticate::class);
 
 $app->post(
     '/activity/edit/{id:\d+}',
     [ActivitiesController::class, 'doEdit']
-)->setName('activities_storeactivity_edit')->add($authenticate);
+)->setName('activities_storeactivity_edit')->add(Authenticate::class);
 
 $app->get(
     '/activity/remove/{id:\d+}',
     [ActivitiesController::class, 'confirmDelete']
-)->setName('activities_remove_activity')->add($authenticate);
+)->setName('activities_remove_activity')->add(Authenticate::class);
 
 $app->post(
     '/activity/remove[/{id:\d+}]',
     [ActivitiesController::class, 'delete']
-)->setName('activities_do_remove_activity')->add($authenticate);
+)->setName('activities_do_remove_activity')->add(Authenticate::class);
 
 //Subscriptions
 $app->get(
     '/subscriptions[/{option:page|order}/{value:\d+}]',
     [SubscriptionsController::class, 'list']
-)->setName('activities_subscriptions')->add($authenticate);
+)->setName('activities_subscriptions')->add(Authenticate::class);
 
 $app->post(
     '/subscriptions/filter',
     [SubscriptionsController::class, 'filter']
-)->setName('activities_filter-subscriptionslist')->add($authenticate);
+)->setName('activities_filter-subscriptionslist')->add(Authenticate::class);
 
 $app->get(
     '/subscription/add[/{id_adh:\d+}]',
     [SubscriptionsController::class, 'add']
 )->setName(
     'activities_subscription_add'
-)->add($authenticate);
+)->add(Authenticate::class);
 
 $app->get(
     '/subscription/edit/{id:\d+}',
     [SubscriptionsController::class, 'edit']
 )->setName(
     'activities_subscription_edit'
-)->add($authenticate);
+)->add(Authenticate::class);
 
 $app->post(
     '/subscription/add',
@@ -99,14 +100,14 @@ $app->post(
 $app->post(
     '/subscription/store',
     [SubscriptionsController::class, 'doEdit']
-)->setName('activities_storesubscription_edit')->add($authenticate);
+)->setName('activities_storesubscription_edit')->add(Authenticate::class);
 
 $app->get(
     '/subscription/remove/{id:\d+}',
     [SubscriptionsController::class, 'confirmDelete']
-)->setName('activities_remove_subscription')->add($authenticate);
+)->setName('activities_remove_subscription')->add(Authenticate::class);
 
 $app->post(
     '/subscription/remove[/{id:\d+}]',
     [SubscriptionsController::class, 'delete']
-)->setName('activities_do_remove_subscription')->add($authenticate);
+)->setName('activities_do_remove_subscription')->add(Authenticate::class);
